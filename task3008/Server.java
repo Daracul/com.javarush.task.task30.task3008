17e9ee6f24fbbfa826e893324ee7cb8c09923c3c
Alexey Malakhov <au.malakhov@gmail.com>
2017-05-22 15:20:58 +0300

    chat step7

--- task3008/Server.java
+++ task3008/Server.java
@@ -1,9 +1,42 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by amalakhov on 22.05.2017.
  */
 public class Server {
     public static void main(String[] args) {
+        int port=ConsoleHelper.readInt();
+        ServerSocket serverSocket=null;
+        try {
+            serverSocket=new ServerSocket(port);
+            System.out.println("Сервер запущен.");
+            do{
+                new Handler(serverSocket.accept()).start();
+            }
+            while (true);
+        }
+        catch (Exception e){
+            System.out.println(e);
+            if (serverSocket!=null){
+                try{
+                    serverSocket.close();
+                }
+                catch (IOException ex){}
+            }
+        }
+
+
+    }
+
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
