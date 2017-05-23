0d1179c0fc4c7abf2b69d91d280dbeeee4427246
Alexey Malakhov <au.malakhov@gmail.com>
2017-05-23 11:26:28 +0300

    chat step14

--- task3008/client/Client.java
+++ task3008/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by amalakhov on 23.05.2017.
@@ -8,6 +13,38 @@ import com.javarush.task.task30.task3008.Connection;
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected=false;
+
+    protected String getServerAddress(){
+        System.out.println("Введите адрес сервера: ");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        System.out.println("Введите адрес порта: ");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        System.out.println("Введите ваше имя: ");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка во время отправки сообщения!");
+            clientConnected=false;
+        }
+    }
     public class SocketThread extends Thread{
     }
 }
